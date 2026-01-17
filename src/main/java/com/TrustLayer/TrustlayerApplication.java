35592a59f9b7b3dec09bef6be2d384daef958766
Piyush1817 <lakwanisuresh086@gmail.com>
2026-01-17 23:07:37 +0530

    Initial backend commit

---
+++ src/main/java/com/TrustLayer/TrustlayerApplication.java
@@ -0,0 +1,13 @@
+package com.TrustLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrustlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrustlayerApplication.class, args);
+	}
+
+}
